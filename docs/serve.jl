50d0dd33b5ab0b0dd1e224299bd1e52cd3990a93
lorenzoh <lorenz.ohly@gmail.com>
2022-03-28 22:15:52 +0200

    Fix ToC link

--- docs/serve.jl
+++ docs/serve.jl
@@ -7,7 +7,5 @@ using Pollen
 project = include("project.jl")
 
 
-@info "Rewriting documents..."
-Pollen.rewritesources!(project)
 
 Pollen.serve(project, lazy=get(ENV, "POLLEN_LAZY", "false") == "true", format = Pollen.JSON())
